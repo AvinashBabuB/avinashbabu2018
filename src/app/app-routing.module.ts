78b10fef1ca23dc7e4de9e949e9da9b0cc2d094d
IshaUser <avinashbabu.boora@ishafoundation.org>
2018-10-08 15:04:03 +0530

    Initial skeleton

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {AboutpageComponent} from './aboutpage/aboutpage.component'
+import { HomepageComponent } from './homepage/homepage.component';
+
+
+const routes: Routes = [
+  { path: 'about', component: AboutpageComponent },
+  { path: '.', component: HomepageComponent }
+];
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [ RouterModule.forRoot(routes) ]
+  
+})
+export class AppRoutingModule { }
